558dc0b92dd46c6a81ee87f1fa204f36fb58a47b
Jordan Harband <ljharb@gmail.com>
2022-11-03 20:45:08 -0700

    [Deps] update `es-abstract`, `get-intrinsic`

--- implementation.js
+++ implementation.js
@@ -2,15 +2,15 @@
 
 var GetIntrinsic = require('get-intrinsic');
 
-var ArraySpeciesCreate = require('es-abstract/2021/ArraySpeciesCreate');
-var CreateDataPropertyOrThrow = require('es-abstract/2021/CreateDataPropertyOrThrow');
-var Get = require('es-abstract/2021/Get');
-var HasProperty = require('es-abstract/2021/HasProperty');
-var LengthOfArrayLike = require('es-abstract/2021/LengthOfArrayLike');
-var Set = require('es-abstract/2021/Set');
-var ToIntegerOrInfinity = require('es-abstract/2021/ToIntegerOrInfinity');
-var ToObject = require('es-abstract/2021/ToObject');
-var ToString = require('es-abstract/2021/ToString');
+var ArraySpeciesCreate = require('es-abstract/2022/ArraySpeciesCreate');
+var CreateDataPropertyOrThrow = require('es-abstract/2022/CreateDataPropertyOrThrow');
+var Get = require('es-abstract/2022/Get');
+var HasProperty = require('es-abstract/2022/HasProperty');
+var LengthOfArrayLike = require('es-abstract/2022/LengthOfArrayLike');
+var Set = require('es-abstract/2022/Set');
+var ToIntegerOrInfinity = require('es-abstract/2022/ToIntegerOrInfinity');
+var ToObject = require('es-abstract/2022/ToObject');
+var ToString = require('es-abstract/2022/ToString');
 
 var callBound = require('call-bind/callBound');
 
